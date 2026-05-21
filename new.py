bbcf5aeb1919e2cc4a0013ee5eef2a18d92c79c2
Flo Greistorfer <flo.greistorfer@sorogon.eu>
2026-05-21 11:49:08 +0200

    correct name

--- new.py
+++ new.py
@@ -51,7 +51,11 @@ with open(os.path.join(name, "build.spec"), encoding="utf-8") as f:
     buildspec = f.read()
 
 with open(os.path.join(name, "build.spec"), "w", encoding="utf-8") as f:
-    f.write(buildspec.replace("src/baseline/main.py", f"src/{name}/main.py").replace("baseline.modules", f"{name}.modules"))
+    f.write(
+        buildspec.replace("src/baseline/main.py", f"src/{name.replace('-', '_')}/main.py").replace(
+            "baseline.modules", f"{name.replace('-', '_')}.modules"
+        )
+    )
 
 with open(os.path.join(name, f"{name}.code-workspace"), encoding="utf-8") as f:
     workspace = json.loads(f.read())
